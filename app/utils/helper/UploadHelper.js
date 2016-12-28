631dea4fc61a196d7f84bbf45b6beceb0b139d82
Michael Braun <michaelsoftware1997@gmail.com>
2016-12-28 23:44:53 +0100

    Added UploadHelper for managing asynchronous uploads

---
+++ app/utils/helper/UploadHelper.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+
+class UploadHelper {
+    constructor(socketHelper) {
+        this.uploaded = {};
+        this.uploadListeners = {};
+        this.socketHelper = socketHelper;
+    }
+
+    setUploaded(id, path) {
+        if(this.uploadListeners[id] && this.uploadListeners[id].success && this.uploadListeners[id].error) {
+            this.uploadListeners[id].success({
+                path: path,
+                id: id
+            });
+        }
+
+        this.uploaded[id] = {
+            path: path,
+            id: id
+        };
+    }
+
+    getUploaded(id) {
+        return new Promise((resolve, reject) => {
+            if(this.uploaded[id]) {
+                resolve(this.uploaded[id]);
+            } else {
+                this.listenUpload(id, resolve, reject);
+            }
+        });
+    }
+
+    listenUpload(id, success, error) {
+        this.uploadListeners[id] = {
+            success: success,
+            error: error
+        };
+    }
+
+    remove(uploadId) {
+        let id = this.socketHelper.getLoginHelper().getId();
+    }
+
+    removeAll() {
+        let id = (this.socketHelper.getLoginHelper()).getId();
+
+        UploadHelper.deleteFolderRecursive('temp/' + id);
+    }
+
+    static deleteFolderRecursive (path) {
+        if(!path.startsWith('temp/')) return;
+
+        if( fs.existsSync(path) ) {
+            fs.readdirSync(path).forEach(function(file,index){
+                var curPath = path + "/" + file;
+                if(fs.lstatSync(curPath).isDirectory()) { // recurse
+                    UploadHelper.deleteFolderRecursive(curPath);
+                } else { // delete file
+                    fs.unlinkSync(curPath);
+                }
+            });
+
+            try {
+                fs.rmdirSync(path);
+            } catch(e) {
+                if(e.code == 'ENOTEMPTY') UploadHelper.deleteFolderRecursive(path);
+            }
+        }
+    };
+}
+
+module.exports = UploadHelper;
