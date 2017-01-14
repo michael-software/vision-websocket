1fa2e5644555ee95967754dea4a7036edc5ba0fd
Michael Braun <michaelsoftware1997@gmail.com>
2017-01-14 02:04:43 +0100

    Added openUrl-action

--- app/utils/jui/models/Action.js
+++ app/utils/jui/models/Action.js
@@ -1,5 +1,11 @@
 module.exports = {
 	submit: function(url) {
 		return `submit(${url || ''})`;
+	},
+
+	openUrl: function(url) {
+		if(url) return `openUrl(${url || ''})`;
+
+		return null;
 	}
 };
