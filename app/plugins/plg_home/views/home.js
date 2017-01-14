004456c4ccec4797623c258a17918da7625c6a58
Michael Braun <michaelsoftware1997@gmail.com>
2017-01-14 02:05:16 +0100

    Updated home.js to use more elements

--- app/plugins/plg_home/views/home.js
+++ app/plugins/plg_home/views/home.js
@@ -37,6 +37,24 @@ console.log(juiHelper);
 	checkbox.setValue(false);
 	checkbox.setLabel('Teste mich!');
 
+	let input2 = new juiHelper.File('test');
+	input2.setLabel('Test');
+	input2.setMultiple(true);
+
+	let entry = new juiHelper.List.Entry('Yahoo');
+	entry.setClick(juiHelper.Action.openUrl('https://www.yahoo.de'));
+
+	let list1 = new juiHelper.List();
+	list1.add(entry);
+	list1.add('Google', juiHelper.Action.openUrl('https://www.google.de'), juiHelper.Action.openUrl('https://www.bing.de'));
+
+	let image = new juiHelper.Image("http://placehold.it/350x150x");
+
+
+	let select = new juiHelper.Select('hiho--select');
+	select.add('Test');
+	select.add('Hallo', 'Test');
+
 
 	juiHelper.add(headline);
 	juiHelper.nline(2);
@@ -45,6 +63,10 @@ console.log(juiHelper);
 	juiHelper.add(input1);
 	juiHelper.add(button);
 	juiHelper.add(checkbox);
+	juiHelper.add(input2);
+	juiHelper.add(list1);
+	juiHelper.add(image);
+	juiHelper.add(select);
 
 	return juiHelper.getArray();
 };
