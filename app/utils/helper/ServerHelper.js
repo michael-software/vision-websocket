44bc116737d7ee6d045bd6c4ad13a26af2dba0cc
Michael Braun <michaelsoftware1997@gmail.com>
2017-01-22 00:30:18 +0100

    Added ServerHelper for Server-Start

---
+++ app/utils/helper/ServerHelper.js
@@ -0,0 +1,38 @@
+const configLoader = require('./configLoader');
+const pluginLoader = require('./PluginHelper/pluginLoader');
+const userLoader = require('./UserHelper/userLoader');
+
+module.exports = class ServerHelper {
+	constructor() {
+
+	}
+
+	init() {
+		return new Promise((resolve, reject) => {
+
+			configLoader().then((config) => {
+
+				if(!config) return reject();
+
+				let pluginPromise = pluginLoader();
+				let userPromise = userLoader(config);
+
+				Promise.all([pluginPromise, userPromise]).then((data) => {
+					if(data[0] && data[1]) {
+						return resolve({
+							config: config,
+							pluginList: data[0],
+							userList: data[1]
+						});
+					}
+
+					return reject();
+				}).catch((error) => {
+					return reject(error);
+				});
+
+			});
+
+		});
+	}
+};
