41bf1d542d25960262792dba9360a3c8d29c4acb
Michael Braun <michaelsoftware1997@gmail.com>
2017-01-22 00:30:39 +0100

    Added ServerHelper to index.js

--- app/index.js
+++ app/index.js
@@ -1,10 +1,14 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const fs = require('fs');
 
-var LoginHelper = require('./utils/helper/LoginHelper.jsx');
-var SocketHelper = require('./utils/helper/SocketHelper');
+const pluginLoader = require('./utils/helper/PluginHelper/pluginLoader.js');
+const userLoader = require('./utils/helper/UserHelper/userLoader.js');
+const LoginHelper = require('./utils/helper/LoginHelper.jsx');
+const SocketHelper = require('./utils/helper/SocketHelper');
+
+const ServerHelper = require('./utils/helper/ServerHelper');
 
 var __dirname = './public/';
 
@@ -15,46 +19,55 @@ try {
     if ( e.code != 'EEXIST' ) throw e;
 }
 
+let serverHelper = new ServerHelper();
 
-http.listen(3000, function(){
-    console.info('\x1b[36m%s\x1b[0m', 'listening on *:3000');
-});
-
-io.on('connection', function(socket){
-    let socketHelper = new SocketHelper(socket);
-	let loginHelper = new LoginHelper(socketHelper);
-
-    console.info('\x1b[36m%s\x1b[0m', 'user connected');
-
-    socket.on('disconnect', function(){
-        console.info('\x1b[36m%s\x1b[0m', 'user disconnected');
-    });
+serverHelper.init().then((data) => {
+	if(data.config && data.userList && data.pluginList) {
+		let serverConfig = data.config;
+		let userList = data.userList;
+		let pluginList = data.pluginList;
 
 
-	socketHelper.register(loginHelper);
-    loginHelper.on('login', (user) => {
-        user.status = 200;
-        socket.emit('loginstatus', user);
-
-        socketHelper.register(loginHelper, user);
-    });
-    loginHelper.on('unauthorized', (data) => {
-        socket.emit('loginstatus', {
-			head: {
-				status:401
-			}
+		http.listen(3000, function () {
+			console.info('\x1b[36m%s\x1b[0m', 'listening on *:3000');
 		});
-    });
+
+		io.on('connection', function (socket) {
+			let socketHelper = new SocketHelper(socket, serverConfig, pluginList, userList);
+			let loginHelper = new LoginHelper(socketHelper);
+
+			console.info('\x1b[36m%s\x1b[0m', 'user connected');
+
+			socket.on('disconnect', function () {
+				console.info('\x1b[36m%s\x1b[0m', 'user disconnected');
+			});
 
 
+			socketHelper.register(loginHelper);
+			loginHelper.on('login', (user) => {
+				user.status = 200;
+				socket.emit('loginstatus', user);
 
-    socket.on('login', function(data) {
-        if(data.server && data.bearer) {
-			console.log('	\x1b[33m%s\x1b[0m:', 'login with token');
-            loginHelper.loginToken(data.server, data.bearer);
-        } else if(data.server && data.username && data.password) {
-            console.log('	\x1b[33m%s\x1b[0m:', 'login with credentials');
-            loginHelper.loginCredentials(data.server, data.username, data.password);
-        }
-    })
+				socketHelper.register(loginHelper, user);
+			});
+			loginHelper.on('unauthorized', (data) => {
+				socket.emit('loginstatus', {
+					head: {
+						status: 401
+					}
+				});
+			});
+
+
+			socket.on('login', function (data) {
+				if (data.server && data.bearer) {
+					console.log('	\x1b[33m%s\x1b[0m:', 'login with token');
+					loginHelper.loginToken(data.server, data.bearer);
+				} else if (data.server && data.username && data.password) {
+					console.log('	\x1b[33m%s\x1b[0m:', 'login with credentials');
+					loginHelper.loginCredentials(data.server, data.username, data.password);
+				}
+			})
+		});
+	}
 });
