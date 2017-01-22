84f72eda61c61c9dd6f6197ca908bc7e48077d83
Michael Braun <michaelsoftware1997@gmail.com>
2017-01-22 13:42:02 +0100

    Added NotificationHelper to manage the sending of Notifications

---
+++ app/utils/helper/NotificationHelper/NotificationHelper.js
@@ -0,0 +1,24 @@
+const CommunicationHelper = require('../CommunicationHelper');
+const Notification = require('./Notfication');
+
+class NotificationHelper extends CommunicationHelper {
+	constructor(socket, serverConfig, userHelper) {
+		super(socket, serverConfig);
+
+		this.serverConfig = serverConfig;
+		this.userHelper = userHelper;
+		this.Notification = Notification;
+	}
+
+	send(userId, notification) {
+		let user = this.userHelper.getUser(userId);
+
+		if(user && notification instanceof Notification) {
+			super.send(user, notification)
+		}
+	}
+}
+
+NotificationHelper.Notification = Notification;
+
+module.exports = NotificationHelper;
