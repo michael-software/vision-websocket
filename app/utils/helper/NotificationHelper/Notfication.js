5700ed57fb2fb2f8e0e7b51da17e7489ccd39d31
Michael Braun <michaelsoftware1997@gmail.com>
2017-01-22 16:23:43 +0100

    Added Click-Event to Notification

--- app/utils/helper/NotificationHelper/Notfication.js
+++ app/utils/helper/NotificationHelper/Notfication.js
@@ -1,7 +1,17 @@
 module.exports = class Notfication {
-	constructor(title, message) {
+	constructor(title, message, image, action) {
 		this.type = 'notification';
 		this.title = title;
 		this.message = message;
+
+		if(action) {
+			this.action = action;
+		}
+	}
+
+	setClick(action) {
+		if(action) {
+			this.action = action;
+		}
 	}
 };
