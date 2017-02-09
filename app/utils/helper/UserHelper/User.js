5f2e148d29eb15fa1e543c0d2878309529fac93d
Michael Braun <michaelsoftware1997@gmail.com>
2017-02-09 23:10:34 +0100

    Added JSDocs to User-class

--- app/utils/helper/UserHelper/User.js
+++ app/utils/helper/UserHelper/User.js
@@ -1,4 +1,4 @@
-module.exports = class {
+module.exports = class User {
 	constructor(userObject, cpermissions) {
 		this.user = userObject;
 		this.user.customPermissions = new Map();
@@ -6,6 +6,10 @@ module.exports = class {
 		this.sockets = [];
 	}
 
+	/**
+	 * Username of the user
+	 * @returns {string|null} - Returns the username or null (empty user)
+	 */
 	getUsername() {
 		if(this.user && this.user.username) {
 			return this.user.username;
@@ -14,6 +18,10 @@ module.exports = class {
 		return null;
 	}
 
+	/**
+	 * The server of the user
+	 * @returns {string|null} - Returns (when available) the server
+	 */
 	getServer() {
 		if(this.user && this.user.server) {
 			return this.user.server;
@@ -22,6 +30,10 @@ module.exports = class {
 		return null;
 	}
 
+	/**
+	 * Id of the user
+	 * @returns {int|null} - Returns the id or null (empty user)
+	 */
 	getId() {
 		if(this.user && this.user.id) {
 			return this.user.id;
@@ -30,18 +42,38 @@ module.exports = class {
 		return null;
 	}
 
+	/**
+	 * Returns the DisplayName of the user (first and last name or username)
+	 * @returns {string|null} - Returns the display name or null (empty user)
+	 */
 	getDisplayName() {
 		return this.getUsername();
 	}
 
+	/**
+	 * @private
+	 * Sets a custom permission
+	 * @param name {string} - Name of the permission
+	 * @param value {string|null} - Permission set or not (1 and 0)
+	 */
 	setCustomPermission(name, value) {
 		this.user.customPermissions.set(name, value === '1');
 	}
 
+	/**
+	 * Get the value of a custom permission
+	 * @param name {string} - Name of the custom permission
+	 * @returns {boolean|null} - Returns the value or null
+	 */
 	getCustomPermission(name) {
-		this.user.customPermissions.get(name);
+		return this.user.customPermissions.get(name);
 	}
 
+	/**
+	 * Checks whether the user has a permission
+	 * @param name {string} - Name of the permission
+	 * @returns {boolean}
+	 */
 	hasPermission(name) {
 		if(this.user.customPermissions.has(name)) {
 			return this.user.customPermissions.get(name);
@@ -54,6 +86,12 @@ module.exports = class {
 		return false;
 	}
 
+	/**
+	 * @private
+	 * Adds a socketId to the user
+	 * @param socketId {string} - socket.io socket id
+	 * @returns {boolean} - Successful or not
+	 */
 	addSocket( socketId ) {
 		if(this.sockets.indexOf(socketId) === -1) {
 			this.sockets.push(socketId);
@@ -63,6 +101,12 @@ module.exports = class {
 		return false;
 	}
 
+	/**
+	 * @private
+	 * Removes a socketId from the user
+	 * @param socketId {string} - socket.io socket id
+	 * @returns {boolean} - Successful or not
+	 */
 	removeSocket( socketId ) {
 		let index = this.sockets.indexOf(socketId);
 		if(index !== -1) {
@@ -73,10 +117,18 @@ module.exports = class {
 		return false;
 	}
 
+	/**
+	 * Get the socketIds of the user
+	 * @returns {Array} - Array of socketIds
+	 */
 	getSockets() {
 		return this.sockets;
 	}
 
+	/**
+	 * Returns an object that represents the user. The object is not parseable by the user-class
+	 * @returns {{}} - Object that represents the user and is JSON-parseable
+	 */
 	getObject() {
 		return {
 			id: this.getId(),
