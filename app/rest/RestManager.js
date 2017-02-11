f35b2f56d949981fe6e8629fab9be5bdbf337fbe
Michael Braun <michaelsoftware1997@gmail.com>
2017-02-11 17:59:51 +0100

    Added Endpoints to API (Plugins, Plugin, Search)

--- app/rest/RestManager.js
+++ app/rest/RestManager.js
@@ -1,5 +1,6 @@
 const PromiseMiddleware = require('./middlewares/Promises');
 const AuthenticationMiddleware = require('./middlewares/Authentication');
+const PluginHelper = require('../utils/helper/PluginHelper/PluginHelper');
 
 class RestManager {
 	constructor(express, config) {
@@ -7,12 +8,58 @@ class RestManager {
 		express.use(AuthenticationMiddleware(config));
 
 		express.get('/login', function (req, res) {
-			console.log(req.connectionHelper.getUserHelper().getCurrentUser());
-
 			res.promise(new Promise((resolve, reject) => {
+				let currentUser = req.connectionHelper.getUserHelper().getCurrentUser().getObject();
+				currentUser.authtoken = req.connectionHelper.getLoginHelper().getToken();
+
 				resolve({
-					data: req.connectionHelper.getUserHelper().getCurrentUser().getObject()
+					data: currentUser
 				});
+			}));
+		});
+
+		express.get('/plugins', function (req, res) {
+			res.promise(new Promise((resolve, reject) => {
+				let plugins = req.connectionHelper.getPluginHelper().getPlugins().values();
+
+
+				let pluginArray = [];
+				for(let plugin of plugins) {
+					pluginArray.push(plugin);
+				}
+
+				return resolve({
+					data: pluginArray
+				});
+			}));
+		});
+
+		express.get('/plugin', function (req, res) {
+			res.promise(new Promise((resolve, reject) => {
+				let plugins = req.connectionHelper.getPluginHelper().getPlugin(req.query.id, req.query.view, req.query.param);
+
+				plugins.then((data) => {
+					return resolve(data);
+				});
+			}));
+		});
+
+		express.get('/search', function (req, res) {
+			res.promise(new Promise((resolve, reject) => {
+				let query = req.query.query;
+
+				if(req.query.format === 'jui') {
+					req.connectionHelper.getSearchHelper().getSearchView(query).then((data) => {
+						return resolve({
+							data: data.getArray(),
+							head: null
+						});
+					});
+				} else {
+					req.connectionHelper.getSearchHelper().getSearch(query).then((data) => {
+						return resolve(data);
+					});
+				}
 			}));
 		});
 	}
