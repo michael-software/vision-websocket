bf92db452c908531767cd53e249c3dca8cc77a03
Michael Braun <michaelsoftware1997@gmail.com>
2017-02-11 19:18:05 +0100

    Fixed wrong Content-Type on non-JSON data

--- app/rest/middlewares/Promises.js
+++ app/rest/middlewares/Promises.js
@@ -2,12 +2,16 @@ module.exports = function promiseify(request, response, next) {
 	response.promise = function(promise) {
 		let responseText;
 
-		response.setHeader('Content-Type', 'application/json');
-
 		promise.then(function(result) {
+			response.setHeader('Content-Type', 'application/json');
+
 			responseText = JSON.stringify(result);
 			response.send(responseText);
 		}).catch(function(error) {
+			response.setHeader('Content-Type', 'application/json');
+
+			error = error || {};
+
 			responseText = JSON.stringify({
 				error: error.message,
 				stack: error.stack
