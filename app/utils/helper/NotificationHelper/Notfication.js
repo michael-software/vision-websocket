1ebce6a693b6fcb31e3af9f45cd0b3987f358566
Michael Braun <michaelsoftware1997@gmail.com>
2017-02-15 23:10:10 +0100

    Added plugin to Notification

--- app/utils/helper/NotificationHelper/Notfication.js
+++ app/utils/helper/NotificationHelper/Notfication.js
@@ -1,8 +1,9 @@
 module.exports = class Notfication {
-	constructor(title, message, image, action) {
+	constructor(title, message, plugin, image, action) {
 		this.type = 'notification';
 		this.title = title;
 		this.message = message;
+		this.plugin = plugin;
 
 		if(action) {
 			this.action = action;
