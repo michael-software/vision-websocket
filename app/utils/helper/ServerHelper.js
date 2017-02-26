56ce915c186d4709dd3e1e4059c49496ec746035
Michael Braun <michaelsoftware1997@gmail.com>
2017-02-26 12:51:57 +0100

    Added cleanup-function for temporary folders

--- app/utils/helper/ServerHelper.js
+++ app/utils/helper/ServerHelper.js
@@ -45,6 +45,50 @@ module.exports = class ServerHelper {
 				});
 			});
 
+		}).then((config) => {
+			this.registerCleanup(config);
+
+			return config;
 		});
 	}
+
+	registerCleanup(config) {
+		process.on('exit', this.cleanup.bind(this,{config: config, exit: true}));
+		process.on('SIGINT', this.cleanup.bind(this, {config: config, exit: true}));
+	}
+
+	cleanup(options) {
+		const {userList} = options.config;
+		const fileHelper = new FileHelper(null);
+
+		console.info('\x1b[36m%s\x1b[0m', 'Cleaning temporary folders');
+
+		userList.map((user) => {
+			const tmpFolder = fileHelper.getUserTempDirectory(user);
+
+			this.deleteFolderRecursive(tmpFolder);
+		});
+
+		if (options.exit) process.exit();
+	}
+
+	deleteFolderRecursive (path) {
+		if( fs.existsSync(path) ) {
+			fs.readdirSync(path).forEach(function(file,index){
+				var curPath = path + "/" + file;
+				if(fs.lstatSync(curPath).isDirectory()) { // recurse
+					this.deleteFolderRecursive(curPath);
+				} else { // delete file
+					fs.unlinkSync(curPath);
+				}
+			});
+
+			try {
+				fs.rmdirSync(path);
+			} catch(e) {
+				if(e.code == 'ENOTEMPTY') this.deleteFolderRecursive(path);
+			}
+		}
+	};
+
 };
