891a570a681048c6fbae4134b050c29df20f9219
Michael Braun <michaelsoftware1997@gmail.com>
2017-02-26 12:51:07 +0100

    Added move-method Added optional User-object to getUserDirectory-methods

--- app/utils/helper/FileHelper/FileHelper.js
+++ app/utils/helper/FileHelper/FileHelper.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
 const path = require('path');
 const __main = path.dirname( path.dirname(require.main.filename) );
 const __files = path.join(__main, 'files');
+
+const User = require('../UserHelper/User');
 
 const PermissionHelper = require('../PermissionHelper/PermissionHelper');
 
@@ -17,14 +20,37 @@ class FileHelper {
 		return __files;
 	}
 
-	getUserDirectory() {
-		let userId = this.connectionHelper.getUserHelper().getCurrentUser().getId();
-
-		return path.join(FileHelper.getServerFileDirectory(), `user_${userId}`);
+	/**
+	 * Returns the directory of the user
+	 * @param {User|null} [user] - Specify a user or use the current user
+	 * @returns {String} - Path to the directory
+	 */
+	getUserDirectory(user) {
+		if(!user || !(user instanceof User) ) {
+			let userId = this.connectionHelper.getUserHelper().getCurrentUser().getId();
+			return path.join(FileHelper.getServerFileDirectory(), `user_${userId}`);
+		} else {
+			let userId = user.getId();
+			return path.join(FileHelper.getServerFileDirectory(), `user_${userId}`);
+		}
 	}
 
-	getUserFileDirectory() {
-		return path.join(this.getUserDirectory(), 'files');
+	/**
+	 * Returns the temporary directory of the user
+	 * @param {User|null} [user] - Specify a user or use the current user
+	 * @returns {String} - Path to the directory
+	 */
+	getUserTempDirectory(user) {
+		return path.join(this.getUserDirectory(user), 'temp');
+	}
+
+	/**
+	 * Returns the private file directory of the user
+	 * @param {User|null} [user] - Specify a user or use the current user
+	 * @returns {String} - Path to the directory
+	 */
+	getUserFileDirectory(user) {
+		return path.join(this.getUserDirectory(user), 'files');
 	}
 
 	isAllowed(type) {
@@ -44,8 +70,31 @@ class FileHelper {
 
 		return new CustomFile(path.join(userFileDir, name));
 	}
+
+	move(from, to) {
+		return new Promise((resolve, reject) => {
+			try {
+				console.log(from);
+
+				let source = fs.createReadStream(from),
+					destination = fs.createWriteStream(to);
+
+
+				source.pipe(destination, {end: false});
+				source.on("end", function () {
+					fs.unlinkSync(from);
+
+
+					return resolve(to);
+				});
+			} catch(error) {
+				return reject(error);
+			}
+		});
+	}
 }
 
 FileHelper.TYPE_PRIVATE = 'private';
+FileHelper.TYPE_TEMP = 'temp';
 
 module.exports = FileHelper;
