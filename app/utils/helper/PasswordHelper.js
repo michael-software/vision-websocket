babf1a1b25d72de8228538f195bfc60cd746f76d
Michael Braun <michaelsoftware1997@gmail.com>
2017-03-01 00:01:39 +0100

    Added bcrypt and PasswordHelper

---
+++ app/utils/helper/PasswordHelper.js
@@ -0,0 +1,19 @@
+const bcrypt = require('bcrypt');
+
+module.exports = class PasswordHelper {
+	static getHash(password, level=5) {
+
+		return new Promise((resolve, reject) => {
+			bcrypt.hash(password, level, function(err, hash) {
+				if(err) return reject(err);
+
+				return resolve(hash);
+			});
+		});
+	}
+
+	static compare(password, hash) {
+		console.log(password, hash);
+		return bcrypt.compareSync(password, hash);
+	}
+};
